ddece553dead76b4618610ed4db84c8ce685b815
Quy <quy.nguyenngoctp@gmail.com>
2017-08-03 19:04:59 +0700

    test button contact_us

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def contact_us
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # get "home/index"
+  
+  get "contact_us" => "home#contact_us"
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
